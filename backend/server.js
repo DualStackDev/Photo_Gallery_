d35deb664b4145a5e638c9a204a5d95aadef66a2
kshitij kardile <kshitijkardile@gmail.com>
2025-06-11 11:04:19 +0530

    allowed origins changed

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,10 @@ const PORT = process.env.PORT || 5000;
 // Define allowed frontend URLs
 const allowedOrigins = [
   "https://photogallery-eosin-ten.vercel.app",
-  "https://photodisplay-tau.vercel.app",
+  "https://aryanportfolio-iota-topaz.vercel.app",
   "http://localhost:3000",
   "http://localhost:3001",
+  "https://aryanagarwal.com",
 ];
 
 app.use(
